a7a1cf11c6aa1ef095fb4ad9b8ed591ff0a11ab7
attkuu <attkuu@gmail.com>
2026-01-18 16:05:55 +0100

    added more comments and pyfiglet banner

--- xortool.py
+++ xortool.py
@@ -3,12 +3,17 @@
 """Xor Obfuscation-tool
 
 This tool obfuscates files using a single-byte XOR operation. It supports
-multiple output formats including raw hex, byte strings, and C-style arrays.
+multiple output formats.
 """
 
 import sys
 import argparse
 from pathlib import Path
+import pyfiglet
+
+# Print banner with pyfiglet
+result = pyfiglet.figlet_format("XOR OBFUSCATOR", font="small")
+print(result)
 
 def main():
     """
@@ -16,16 +21,18 @@ def main():
     Command-line Arguments:
         --input, -i: Path to the file to be obfuscated.
         --output, -o: Path where the obfuscated file will be saved.
-        --key, -k: A 1-byte hex string (e.g., '0xAA' or 'FF') used as the XOR key.
+        --key, -k: A 1-byte hex string ('0xAA' or 'FF' for example) used as the XOR key.
         --format, -f: Optional output display format (python, raw, or c-array).
     """
     parser = argparse.ArgumentParser(description ="A XOR obfuscation tool.")
+    # Required arguments
     parser.add_argument('--input','-i', required=True,dest='input_file',
     help='The name of the input file.')
     parser.add_argument('--output','-o', required=True,dest='output_file',
     help='The name of the output file.')
     parser.add_argument('--key','-k', required=True,dest='input_key',
     help='1 byte hexadecimal string.')
+    # Optional argument
     parser.add_argument('--format','-f', choices=["python", "raw", "c-array"],dest='input_format',
     help='Choose a format to output the ciphertext. Options: python, raw, c-array. [Optional]')
     # Reading input file
@@ -58,23 +65,21 @@ def main():
     for byte in plaintext:
         ciphertext_list.append(byte ^ key_byte)
 
+    # Converting the list to a bytes object
     ciphertext = bytes(ciphertext_list)
 
-    # Printing output
     formatinput = args.input_format
+    # Prints a Python list
     if formatinput == "python":
-        # Formats each byte as a hex value inside a standard Python list
-        formatted_list = [f"0x{b:02x}" for b in ciphertext]
-        print(f"Ciphertext (python-list): [{', '.join(formatted_list)}]")
+        formatted = ", ".join([f"0x{b:02x}" for b in ciphertext])
+        print(f"Ciphertext (python-list): [{formatted}]")
+    # Prints raw bytes
     elif formatinput == "raw":
         print(f"Ciphertext (raw bytes): {ciphertext!r}")
+    # Prints a c-array
     elif formatinput == "c-array":
-        hex_string = ciphertext.hex()
-        c_array = ""
-        for i in range(0, len(hex_string), 2):
-            c_array += f"0x{hex_string[i:i+2]}, "
-        c_array = c_array.rstrip(', ')
-        print("C-ARRAY:")
+        c_array = ", ".join([f"0x{b:02x}" for b in ciphertext])
+        print("C-ARRAY:\n")
         print(f"unsigned char xored_shellcode[] = {{ {c_array} }};")
 
     # Saving the output to a file
